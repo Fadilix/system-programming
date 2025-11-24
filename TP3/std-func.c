e363b5a9974d63a5dfa1c48c41070bd1d8d871c8
Fadilix <iamfadile2@gmail.com>
2025-11-24 04:14:45 +0100

    feat: palindrom

---
+++ TP3/std-func.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+void strcpy_remake(char s1[], char s2[]) {}
+void strcmp_remake(char s1[], char s2[]) {}
+void strupper_remake(char s1[], char s2[]) {}
+
+int main() { return 0; }
